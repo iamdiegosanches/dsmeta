b87a28daf7cc55e53ba580b37a9506146c41bd8b
Diego <74864968+iamdiegosanches@users.noreply.github.com>
2022-07-14 17:19:04 -0300

    Other components

---
+++ frontend/src/components/Header/index.tsx
@@ -0,0 +1,22 @@
+import logo from '../../assets/img/header-icon.svg'
+
+import './styles.css'
+
+function Header() {
+    return (
+        <>
+            <header>
+                <div className="dsmeta-logo-container">
+                    <img src={logo} alt="DSMeta"/>
+                    <h1>DSMeta</h1>
+                    <p>
+                        Desenvolvido por
+                        <a href="https://github.com/iamdiegosanches">iamdiegosanches</a>
+                    </p>
+                </div>
+            </header>
+        </>
+    )
+}
+
+export default Header
